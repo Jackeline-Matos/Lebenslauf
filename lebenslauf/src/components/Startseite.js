fae3fa655e8afaf8d627b0c82e8a00c2ab3be657
Amna El Sayed <amnaelsayed2@gmail.com>
2022-10-19 14:54:44 +0200

    formular

--- lebenslauf/src/components/Startseite.js
+++ lebenslauf/src/components/Startseite.js
@@ -1,8 +1,10 @@
 import "./Startseite.css";
 import { motion } from "framer-motion";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useState } from "react";
 const Startseite = () => {
   const [move, setMove] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className="background">
       <div className="center">
@@ -10,10 +12,14 @@ const Startseite = () => {
         <motion.button
           animate={{ rotate: move ? 360 : 0 }}
           // transition={{ type: "tween", duration: 2 }}
-          // transition={{ type: "spring", duration: 2 }}
-          transition={{ type: "inerita", velocity: 40 }}
-          // transition={{ type: "bounce", duration: 2 }}
-          onClick={() => setMove(!move)}
+          transition={{ type: "spring", bounce: 5 }}
+          // transition={{ type: "inerita", velocity: 40 }}
+
+          onClick={() => {
+            setMove(!move);
+            const nav = () => navigate("/formular");
+            nav();
+          }}
           className="button"
         >
           GET STARTED
