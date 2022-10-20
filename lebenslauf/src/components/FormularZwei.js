4f0a3c032d32899873f1c0f0971db3161679d31c
Jackeine-Matos <jackeline.matos@gmail.com>
2022-10-20 12:45:33 +0200

    bearbeitungformular

--- lebenslauf/src/components/FormularZwei.js
+++ lebenslauf/src/components/FormularZwei.js
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import "./Formular.css";
+import { useState } from "react";
 const FormularZwei = () => {
   return (
     <motion.div
@@ -6,24 +8,66 @@ const FormularZwei = () => {
       animate={{ width: "100vw" }}
       exit={{ x: "window.innerWidth", transition: { duration: 0.1 } }}
     >
-      <h1>Shulbildung</h1>
+      <h1>Schulbildung</h1>
 
       <form>
         <div className="zeile">
           <div className="flex">
-            <label htmlFor=""></label>
-            <select>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option selected value="coconut">
-                Coconut
-              </option>
-              <option value="mango">Mango</option>
-            </select>
+            <label htmlFor="ausbildung">Schulform</label>
+            <input type="text" id="ausbildung" />
           </div>
           <div className="flex">
-            <label htmlFor="vorname">Vorname</label>
-            <input type="text" id="vorname" />
+            <label htmlFor="vorname">Schule</label>
+            <input type="text" id="schule" />
+          </div>
+          <div className="flex-eins">
+            <label>von</label>
+            <input type="date" id="start-schule" />
+          </div>
+
+          <div className="flex-eins">
+            <label>bis</label>
+            <input type="date" id="end-schule" />
+          </div>
+        </div>
+
+        <div className="zeile">
+          <div className="flex">
+            <label htmlFor="ausbildung">Studium</label>
+            <input type="text" id="bachelor" />
+          </div>
+          <div className="flex">
+            <label htmlFor="universitat">Universität</label>
+            <input type="text" id="universitat" />
+          </div>
+          <div className="flex-eins">
+            <label>Von</label>
+            <input type="date" id="start-universitat" />
+          </div>
+
+          <div className="flex-eins">
+            <label>bis</label>
+            <input type="date" id="end-universitat" />
+          </div>
+        </div>
+
+        <div className="zeile">
+          <div className="flex-zwei">
+            <label>Abschluss</label>
+            <select>
+              <option value="grapefruit"></option>
+              <option value="lime"></option>
+              <option selected value="coconut"></option>
+              <option value="mango"></option>
+            </select>
+          </div>
+          <div className="flex-zwei">
+            <label>Studiengang</label>
+            <input type="text" id="universitat" />
+          </div>
+          <div className="flex-zwei">
+            <label>am</label>
+            <input type="date" id="end-universitat" />
           </div>
         </div>
       </form>
