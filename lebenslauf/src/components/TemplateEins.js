53cf69e704380b80a9b52b33f549af0c599f8669
Amna El Sayed <amnaelsayed2@gmail.com>
2022-10-24 13:18:16 +0200

    conditionalRender

--- lebenslauf/src/components/TemplateEins.js
+++ lebenslauf/src/components/TemplateEins.js
@@ -47,15 +47,39 @@ const TemplateEins = () => {
         style={{ width: "100%", height: window.innerHeight }}
       >
         <div className="containerTemplateEins">
-          <div className="header">header</div>
-          <div className="untertitel">untertitel</div>
-          <div className="contentLinksTemplateEins">Persönliches Profil</div>
-          <div className="contentRechtsTemplateEins">Beruflicher Werdegang</div>
+          <div className="header">{user.vorname}</div>
+          <div className="untertitel">{user.stellen}</div>
+          <div className="contentLinksTemplateEins">
+            <div> Persönliches Profil</div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
+              doloremque mollitia veritatis dolore, culpa ut, nihil ipsam
+              expedita, impedit distinctio ea accusantium velit maxime? Optio
+              neque corrupti eaque omnis odio!
+            </p>
+          </div>
+          <div className="contentRechtsTemplateEins">
+            <div>Beruflicher Werdegang</div>
+            <ul>
+              {beruf.map((item) => (
+                <>
+                  {item.stellen.length > 1 ? (
+                    <>
+                      <li>{item.stellen} </li>{" "}
+                      <li>
+                        {item.unternehmen}- {item.start}-{item.end}
+                      </li>
+                    </>
+                  ) : null}
+                </>
+              ))}
+            </ul>
+          </div>
           <div className="faehigkeiten">faehigkeiten</div>{" "}
           <div className="kontaktinformationen">Kontaktinformationen</div>
           <div className="schulbildung">Schulbildung</div>
           <div className="hobbiesTemplateEins">Hobbys</div>
-          <div className="footerTemplateEins">Footer</div>
+          <div className="footerTemplateEins">{user.name}</div>
         </div>
         <button onClick={handlePrint}>Print</button>
       </div>
